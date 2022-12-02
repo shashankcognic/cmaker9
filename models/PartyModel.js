72879efc677e46a4c41e32ff1cced2b0c515130d
Shashankcognic <shashank.patel@cognicsys.com>
2022-12-02 11:33:28 +0530

    first commit

---
+++ models/PartyModel.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema } from "mongoose";
+
+const partyDataSchema = new mongoose.Schema(
+  {
+    partyname: {
+      type: String,
+      
+    },
+    email: {
+      type: String,
+    
+    },
+    bankname:{type:String},
+    branch:{type:String},
+  
+    address: {
+      type: Schema.Types.Mixed,
+    },
+    city: {
+      type: String,
+    
+    },
+    state: {
+      type: String,
+      
+    },
+    pincode: {
+      type: Number,
+      
+    },
+    phonenumber: {
+      type: Number,
+      
+    },
+    phonenumber2: {
+      type: String,
+    
+    },
+
+    userid: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "userData",
+      
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+const partyData = mongoose.model("paryData", partyDataSchema);
+export default partyData;
